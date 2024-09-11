7c63983ebc1edacf2ec6d0d8f6319ba62025439b
Ranon Emerick <ranonemerick@gmail.com>
2024-09-11 11:21:39 -0300

    Ajuste para melhorar a busca ignorando maiusculas e minusculas

--- src/main/java/br/com/sistematizacao/POO_II/specification/ContactSpecification.java
+++ src/main/java/br/com/sistematizacao/POO_II/specification/ContactSpecification.java
@@ -10,18 +10,20 @@ import org.springframework.data.jpa.domain.Specification;
 public class ContactSpecification {
 
     public static Specification<Contact> param(String name, String phoneNumber) {
-        return (Root<Contact> root, CriteriaQuery<?> query, CriteriaBuilder builder ) -> {
+        return (Root<Contact> root, CriteriaQuery<?> query, CriteriaBuilder builder) -> {
             Predicate p = builder.conjunction();
 
-            if(name != null) {
-                p = builder.and(p, builder.like(root.get("name"), "%" + name + "%"));
+            if (name != null && !name.trim().isEmpty()) {
+                String modifiedName = name.replace("-", " ").toLowerCase();
+                p = builder.and(p, builder.like(builder.function("lower", String.class, root.get("name")), "%" + modifiedName + "%"));
             }
 
-            if(phoneNumber != null) {
+            if (phoneNumber != null && !phoneNumber.trim().isEmpty()) {
                 p = builder.and(p, builder.like(root.get("phoneNumber"), "%" + phoneNumber + "%"));
             }
+
             return p;
         };
     }
-
 }
+
